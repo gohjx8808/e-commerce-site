224d623e118dcfb29e178ee21dfd43a114c9d052
Jing Xuan Goh <jingxuan.goh@capbay.com>
2021-06-30 22:14:12 +0800

    integrated redux saga

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { applyMiddleware, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from './rootReducer';
+import rootSaga from './rootSaga';
 
-const store = configureStore({
-  reducer: {},
-});
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(sagaMiddleware),
+);
+
+sagaMiddleware.run(rootSaga);
 
 export type RootState=ReturnType<typeof store.getState>
 
