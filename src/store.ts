43dedc016459d33bd69e8e636f00e0b2c09a2164
Jing Xuan Goh <jingxuan.goh@capbay.com>
2021-07-15 10:00:05 +0800

    integrated persisted redux state

--- src/store.ts
+++ src/store.ts
@@ -1,14 +1,23 @@
 import { applyMiddleware, createStore } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { loadState, saveState } from './utils/localStorageUtils';
 import rootReducer from './rootReducer';
 import rootSaga from './rootSaga';
 
 const sagaMiddleware = createSagaMiddleware();
 
+const persistedState = loadState();
+
 const store = createStore(
   rootReducer,
+  persistedState,
   applyMiddleware(sagaMiddleware),
 );
+
+store.subscribe(() => {
+  const state = store.getState();
+  saveState(state);
+});
 
 sagaMiddleware.run(rootSaga);
 
