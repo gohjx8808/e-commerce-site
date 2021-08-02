5f22ccbfc902df7cb065207ebb56bdda947db41a
Jing Xuan Goh <jingxuan.goh@capbay.com>
2021-08-02 20:43:46 +0800

    enahance filtering

--- src/modules/products/views/Products.tsx
+++ src/modules/products/views/Products.tsx
@@ -2,16 +2,21 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { graphql, useStaticQuery } from 'gatsby';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Link } from 'react-scroll';
 import { useAppSelector } from '../../../hooks';
 import productStyle from '../src/productStyle';
 import ProductCard from './ProductCard';
 
+interface categoryAmountData{
+  [key:string]:number
+}
+
 const Products = () => {
   const styles = productStyle();
   const productFilterKeyword = useAppSelector((state) => state.product.productFilterKeyword);
   const [categories, setCategories] = useState<string[]>([]);
+  const [categoryProductAmount, setCategoryProductAmount] = useState<categoryAmountData>({});
   const allProducts:products.rawProductQueryData = useStaticQuery(graphql`
     query ProductPrices {
       allContentfulProducts(filter: {node_locale: {eq: "en-US"}}) {
@@ -32,31 +37,48 @@ const Products = () => {
       }
     }`);
 
+  const filterProductKeyword = useCallback(
+    (productName:string) => productName.toLowerCase().includes(
+      productFilterKeyword.toLowerCase(),
+    ), [productFilterKeyword],
+  );
+
   useEffect(() => {
     const categoryList = [] as string[];
+    const categoryAmount = {} as categoryAmountData;
     allProducts.allContentfulProducts.edges.map((product) => {
       const productCategory = product.node.category;
-      if (productCategory && !categoryList.includes(productCategory)) {
-        categoryList.push(productCategory);
+      if (productCategory) {
+        if (filterProductKeyword(product.node.name)) {
+          if (categoryAmount[productCategory]) {
+            categoryAmount[productCategory] += 1;
+          } else {
+            categoryAmount[productCategory] = 1;
+          }
+        }
+        if (!categoryList.includes(productCategory)) {
+          categoryList.push(productCategory);
+        }
       }
       return null;
     });
+    setCategoryProductAmount(categoryAmount);
     setCategories(categoryList);
-  }, [allProducts]);
+  }, [allProducts, filterProductKeyword]);
 
   const filterProduct = (product:products.innerProductQueryData, category:string) => {
     let isProductInFilter = true;
     let isProductInCategory = true;
     if (productFilterKeyword) {
-      isProductInFilter = product.node.name.toLowerCase().includes(
-        productFilterKeyword.toLowerCase(),
-      );
+      isProductInFilter = filterProductKeyword(product.node.name);
     }
     if (product.node.category) {
       isProductInCategory = product.node.category === category;
     }
     return isProductInFilter && isProductInCategory;
   };
+
+  console.log(categoryProductAmount);
 
   return (
     <>
@@ -67,7 +89,7 @@ const Products = () => {
       ))}
       {categories.map((category) => (
         <Grid container justifyContent="center" alignItems="center" direction="column" key={category}>
-          <Typography variant="h6" id={category} className={styles.categorySpacing}>{category}</Typography>
+          {categoryProductAmount[category] && <Typography variant="h6" id={category} className={styles.categorySpacing}>{category}</Typography>}
           <Grid container justifyContent="center" alignItems="center" direction="row" spacing={5} key={category}>
             {allProducts.allContentfulProducts.edges.filter(
               (product) => filterProduct(product, category),
