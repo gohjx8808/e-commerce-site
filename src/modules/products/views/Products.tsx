53369972a776dba0b025019a671c1f961ed06596
Jing Xuan Goh <jingxuan.goh@capbay.com>
2021-08-02 10:24:45 +0800

    integrated react-scroll

--- src/modules/products/views/Products.tsx
+++ src/modules/products/views/Products.tsx
@@ -1,11 +1,15 @@
+import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 import { graphql, useStaticQuery } from 'gatsby';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-scroll';
 import { useAppSelector } from '../../../hooks';
 import ProductCard from './ProductCard';
 
 const Products = () => {
   const productFilterKeyword = useAppSelector((state) => state.product.productFilterKeyword);
+  const [categories, setCategories] = useState<string[]>([]);
   const allProducts:products.rawProductQueryData = useStaticQuery(graphql`
     query ProductPrices {
       allContentfulProducts(filter: {node_locale: {eq: "en-US"}}) {
@@ -26,21 +30,52 @@ const Products = () => {
       }
     }`);
 
-  const filterProduct = (product:products.innerProductQueryData) => {
+  useEffect(() => {
+    const categoryList = [] as string[];
+    allProducts.allContentfulProducts.edges.map((product) => {
+      const productCategory = product.node.category;
+      if (productCategory && !categoryList.includes(productCategory)) {
+        categoryList.push(productCategory);
+      }
+      return null;
+    });
+    setCategories(categoryList);
+  }, [allProducts]);
+
+  const filterProduct = (product:products.innerProductQueryData, category:string) => {
+    let isProductInFilter = true;
+    let isProductInCategory = true;
     if (productFilterKeyword) {
-      return product.node.name.toLowerCase().includes(productFilterKeyword.toLowerCase());
+      isProductInFilter = product.node.name.toLowerCase().includes(
+        productFilterKeyword.toLowerCase(),
+      );
     }
-    return true;
+    if (product.node.category) {
+      isProductInCategory = product.node.category === category;
+    }
+    return isProductInFilter && isProductInCategory;
   };
 
   return (
-    <Grid container justifyContent="center" alignItems="center" direction="row" spacing={5}>
-      {allProducts.allContentfulProducts.edges.filter(
-        (product) => filterProduct(product),
-      ).map((product) => (
-        <ProductCard key={product.node.contentful_id} product={product.node} />
+    <>
+      {categories.map((category) => (
+        <Link to={category} key={category} spy smooth offset={-60}>
+          <Button>{category}</Button>
+        </Link>
       ))}
-    </Grid>
+      {categories.map((category) => (
+        <Grid container justifyContent="center" alignItems="center" direction="column" key={category}>
+          <Typography variant="h6" id={category}>{category}</Typography>
+          <Grid container justifyContent="center" alignItems="center" direction="row" spacing={5} key={category}>
+            {allProducts.allContentfulProducts.edges.filter(
+              (product) => filterProduct(product, category),
+            ).map((product) => (
+              <ProductCard key={product.node.contentful_id} product={product.node} />
+            ))}
+          </Grid>
+        </Grid>
+      ))}
+    </>
   );
 };
 
