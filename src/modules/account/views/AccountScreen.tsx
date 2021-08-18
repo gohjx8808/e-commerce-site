6f8714da24c5d96158344a2d21a120840bfc698c
Jing Xuan Goh <jingxuan.goh@capbay.com>
2021-08-18 20:22:05 +0800

    fix tab indicator not set at first load

--- src/modules/account/views/AccountScreen.tsx
+++ src/modules/account/views/AccountScreen.tsx
@@ -1,10 +1,12 @@
+import { TabsActions } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import Tab from '@material-ui/core/Tab';
 import TabContext from '@material-ui/lab/TabContext';
 import TabList from '@material-ui/lab/TabList';
 import TabPanel from '@material-ui/lab/TabPanel';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRef } from 'react';
 import CustomBreadcrumbs from '../../../sharedComponents/CustomBreadcrumbs';
 import AccountDetails from './AccountDetails';
 import AddressBook from './AddressBook';
@@ -40,6 +42,16 @@ export default function AccountScreen() {
     setValue(newValue);
   };
 
+  const dekstopTabRef = useRef<TabsActions>(null);
+  const mobileTabRef = useRef<TabsActions>(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      dekstopTabRef.current?.updateIndicator();
+      mobileTabRef.current?.updateIndicator();
+    }, 100);
+  });
+
   return (
     <Grid item xs={12}>
       <Grid item md={10} xs={12}>
@@ -49,11 +61,11 @@ export default function AccountScreen() {
       </Grid>
       <div className={styles.root}>
         <TabContext value={value}>
-          <TabList onChange={handleChange} aria-label="account tabs" orientation="vertical" className={styles.desktopSideTab}>
+          <TabList action={dekstopTabRef} onChange={handleChange} aria-label="account tabs" orientation="vertical" className={styles.desktopSideTab}>
             <Tab label="Account Details" value="accDetails" />
             <Tab label="Address Book" value="addressBook" />
           </TabList>
-          <TabList onChange={handleChange} aria-label="account tabs" variant="scrollable" className={styles.smallerViewTabs}>
+          <TabList action={mobileTabRef} onChange={handleChange} aria-label="account tabs" variant="scrollable" className={styles.smallerViewTabs}>
             <Tab label="Account Details" value="accDetails" />
             <Tab label="Address Book" value="addressBook" />
           </TabList>
