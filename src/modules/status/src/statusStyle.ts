718ad7b038f3af6f63217f070a751170dcd67780
Jing Xuan Goh <jingxuan.goh@capbay.com>
2021-08-20 19:57:31 +0800

    fix status modal zindex

--- src/modules/status/src/statusStyle.ts
+++ src/modules/status/src/statusStyle.ts
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
   backdropRoot: {
-    zIndex: theme.zIndex.drawer + 1,
+    zIndex: theme.zIndex.modal + 1,
   },
   statusBgImgContainer: {
     [theme.breakpoints.up('lg')]: {
