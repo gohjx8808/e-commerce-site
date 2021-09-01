da3d1ce7330e3619a9724183d67c7cdf20646dd7
Jing Xuan Goh <jingxuan.goh@capbay.com>
2021-09-01 22:41:21 +0800

    added send feedback email api

---
+++ src/api/sendGrid/sendFeedbackEmail.ts
@@ -0,0 +1,28 @@
+import sgMail from '@sendgrid/mail';
+import { GatsbyFunctionRequest, GatsbyFunctionResponse } from 'gatsby';
+
+sgMail.setApiKey(process.env.GATSBY_SENDGRID_API_KEY!);
+
+const sendFeedbackEmail = async (req: GatsbyFunctionRequest, res: GatsbyFunctionResponse) => {
+  const postData:feedback.submitFeedbackFormPayload = req.body;
+
+  const email = [{
+    to: 'yj.artjournal@gmail.com',
+    from: 'piggy.chiah@gmail.com',
+    templateId: 'd-3712d484569b41f18e1525a602254adf',
+    dynamic_template_data: {
+      nickname: '',
+      email: postData.email,
+      feedback: postData.feedback,
+    },
+  }];
+
+  try {
+    await sgMail.send(email);
+    return res.status(200).json({ msg: 'Feedback email sent successfully' });
+  } catch (e) {
+    return res.status(e.code).json({ msg: e.message });
+  }
+};
+
+export default sendFeedbackEmail;
