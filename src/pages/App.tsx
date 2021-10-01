560085fdc1683cb7175bb02b56ed63642376f388
Jing Xuan Goh <jingxuan.goh@capbay.com>
2021-10-01 10:05:55 +0800

    remove withstyles usage in app

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,17 +1,16 @@
-import { makeStyles } from '@mui/styles';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Box from '@mui/material/Box';
+import CircularProgress from '@mui/material/CircularProgress';
+import Fab from '@mui/material/Fab';
+import Grid from '@mui/material/Grid';
+import { useTheme } from '@mui/material/styles';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import { RouteComponentProps, Router } from '@reach/router';
 import { navigate } from 'gatsby';
 import React, {
   lazy, Suspense, useEffect, useState,
 } from 'react';
-import { Theme } from '@mui/material/styles';
-import Fab from '@mui/material/Fab';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import Box from '@mui/material/Box';
-import CircularProgress from '@mui/material/CircularProgress';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import Toolbar from '@mui/material/Toolbar';
 import { useAppSelector } from '../hooks';
 import routeNames from '../utils/routeNames';
 
@@ -36,87 +35,60 @@ const Checkout = lazy(() => import('../modules/products/views/Checkout'));
 const EnlargedProductImageCarouselModal = lazy(() => import('../modules/products/views/EnlargedProductImageCarouselModal'));
 const ProductDescription = lazy(() => import('../modules/products/views/ProductDescription'));
 
-const useStyles = makeStyles((theme:Theme) => ({
-  content: {
-    flexGrow: 1,
-  },
-  pageBannerBg: {
-    backgroundColor: !isSSR ? theme.palette.secondary.main : '',
-    padding: 10,
-  },
-  pageBannerText: {
-    color: 'white',
-  },
-  root: {
-    display: 'flex',
-    minHeight: '100vh',
-  },
-  authContent: {
-    flexGrow: 1,
-    display: 'flex',
-    alignItems: 'center',
-    backgroundColor: !isSSR ? theme.palette.primary.main : '',
-  },
-}));
-
 const isSSR = typeof window === 'undefined';
 
-const App = () => {
-  const styles = useStyles();
-  return (
-    <>
-      {!isSSR && (
-        <Suspense fallback={(
-          <Grid container className={styles.root} justifyContent="center" alignItems="center">
-            <CircularProgress color="primary" size={60} />
-          </Grid>
-          )}
-        >
-          <SEO />
-          <Router>
-            <MainRoutes path="/" pageComponent={<HomeScreen />} homeCarouselBanner />
-            <RouterPage path={routeNames.login} pageComponent={<LoginScreen />} />
-            <RouterPage path={routeNames.signUp} pageComponent={<SignupScreen />} />
-            <RouterPage path={routeNames.forgotPassword} pageComponent={<ForgotPassword />} />
-            <MainRoutes path={routeNames.products} pageComponent={<Products />} pageBannerTitle="Product Categories" />
-            <MainRoutes
-              path={routeNames.productDescription}
-              pageComponent={<ProductDescription />}
-            />
-            <MainRoutes path={routeNames.imageGallery} pageComponent={<ImageGallery />} />
-            <MainRoutes path={routeNames.cart} pageComponent={<Cart />} />
-            <MainRoutes path={routeNames.checkout} pageComponent={<Checkout />} />
-            <MainRoutes
-              path={routeNames.account}
-              pageComponent={<AccountScreen />}
-              protectedRoute
-            />
-            <MainRoutes path={routeNames.feedbackForm} pageComponent={<FeedbackForm />} pageBannerTitle="Feedback Form" />
-            <MainRoutes path={routeNames.learnMore} pageComponent={<LearnMore />} />
-          </Router>
-          <StatusModal />
-          <LoadingOverlay />
-          <EnlargedProductImageCarouselModal />
-        </Suspense>
-      )}
-    </>
-  );
-};
+const App = () => (
+  <>
+    {!isSSR && (
+    <Suspense fallback={(
+      <Grid container display="flex" minHeight="100vh" justifyContent="center" alignItems="center">
+        <CircularProgress color="primary" size={60} />
+      </Grid>
+    )}
+    >
+      <SEO />
+      <Router>
+        <MainRoutes path="/" pageComponent={<HomeScreen />} homeCarouselBanner />
+        <RouterPage path={routeNames.login} pageComponent={<LoginScreen />} />
+        <RouterPage path={routeNames.signUp} pageComponent={<SignupScreen />} />
+        <RouterPage path={routeNames.forgotPassword} pageComponent={<ForgotPassword />} />
+        <MainRoutes path={routeNames.products} pageComponent={<Products />} pageBannerTitle="Product Categories" />
+        <MainRoutes
+          path={routeNames.productDescription}
+          pageComponent={<ProductDescription />}
+        />
+        <MainRoutes path={routeNames.imageGallery} pageComponent={<ImageGallery />} />
+        <MainRoutes path={routeNames.cart} pageComponent={<Cart />} />
+        <MainRoutes path={routeNames.checkout} pageComponent={<Checkout />} />
+        <MainRoutes
+          path={routeNames.account}
+          pageComponent={<AccountScreen />}
+          protectedRoute
+        />
+        <MainRoutes path={routeNames.feedbackForm} pageComponent={<FeedbackForm />} pageBannerTitle="Feedback Form" />
+        <MainRoutes path={routeNames.learnMore} pageComponent={<LearnMore />} />
+      </Router>
+      <StatusModal />
+      <LoadingOverlay />
+      <EnlargedProductImageCarouselModal />
+    </Suspense>
+    )}
+  </>
+);
 
 const RouterPage = (
   props: { pageComponent: JSX.Element } & RouteComponentProps,
 ) => {
   const { pageComponent } = props;
-  const styles = useStyles();
+  const theme = useTheme();
+
   return (
-    <div className={styles.root}>
-      <Grid container direction="column">
-        <main className={styles.authContent}>
-          {pageComponent}
-        </main>
-        <Footer />
+    <Grid container minHeight="100vh" direction="column">
+      <Grid alignItems="center" display="flex" flexGrow={1} bgcolor={theme.palette.primary.main}>
+        {pageComponent}
       </Grid>
-    </div>
+      <Footer />
+    </Grid>
   );
 };
 
@@ -129,7 +101,7 @@ const MainRoutes = (props: {
   const {
     pageComponent, pageBannerTitle, homeCarouselBanner, protectedRoute,
   } = props;
-  const styles = useStyles();
+  const theme = useTheme();
   const currentUserDetail = useAppSelector((state) => state.auth.currentUser);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
@@ -150,24 +122,24 @@ const MainRoutes = (props: {
   }
 
   return (
-    <div className={styles.root}>
+    <Box display="flex" minHeight="100vh">
       <MenuBar />
       <Grid container direction="column">
-        <main className={styles.content}>
+        <Grid flexGrow={1}>
           <div id="back-to-top-anchor" />
           <Toolbar />
           {homeCarouselBanner && <HomeBanner />}
           {pageBannerTitle && (
-          <Grid item xs={12} className={styles.pageBannerBg}>
-            <Grid container justifyContent="center" alignItems="center">
-              <Typography variant="h4" className={styles.pageBannerText}>{pageBannerTitle}</Typography>
+            <Grid item xs={12} padding={1} bgcolor={theme.palette.secondary.main}>
+              <Grid container justifyContent="center" alignItems="center">
+                <Typography variant="h4" color="white">{pageBannerTitle}</Typography>
+              </Grid>
             </Grid>
-          </Grid>
           )}
-          <Box sx={{ m: 3 }}>
+          <Box margin={3}>
             {pageComponent}
           </Box>
-        </main>
+        </Grid>
         <Footer />
         <ScrollTop>
           <Fab color="secondary" size="medium" aria-label="scroll back to top">
@@ -175,7 +147,7 @@ const MainRoutes = (props: {
           </Fab>
         </ScrollTop>
       </Grid>
-    </div>
+    </Box>
   );
 };
 
