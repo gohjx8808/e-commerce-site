1510870c844033c3d819a97dbce841e07e055318
Jing Xuan Goh <jingxuan.goh@capbay.com>
2021-10-03 12:43:18 +0800

    clean styledlistitem

--- src/sharedComponents/StyledListItem.tsx
+++ src/sharedComponents/StyledListItem.tsx
@@ -1,19 +1,18 @@
-import ListItemButton from '@mui/material/ListItemButton';
+import ListItemButton, { ListItemButtonProps } from '@mui/material/ListItemButton';
 import { styled } from '@mui/material/styles';
 
-const StyledListItem = styled(ListItemButton)(({ theme }) => ({
+const StyledListItem = styled(ListItemButton)<ListItemButtonProps>(({ theme }) => ({
+  color: theme.palette.text.primary,
   '&.MuiListItemButton-root': {
     '&.Mui-selected': {
       backgroundColor: theme.palette.secondary.main,
-      '& .MuiIconButton-root, & .MuiTypography-root, & .MuiListItemIcon-root': {
+      color: theme.palette.common.white,
+      '& .MuiListItemIcon-root': {
         color: theme.palette.common.white,
       },
     },
   },
   '.MuiListItemIcon-root': {
-    color: theme.palette.text.primary,
-  },
-  '.MuiListItemText-root': {
     color: theme.palette.text.primary,
   },
 }));
