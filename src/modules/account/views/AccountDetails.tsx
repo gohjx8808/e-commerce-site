c344228da7ae3a64429ecca8e4f58b6af1b9eb4b
Jing Xuan Goh <jingxuan.goh@capbay.com>
2021-10-06 22:16:51 +0800

    acc details styled

--- src/modules/account/views/AccountDetails.tsx
+++ src/modules/account/views/AccountDetails.tsx
@@ -4,13 +4,11 @@ import PersonIcon from '@mui/icons-material/Person';
 import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
 import WcIcon from '@mui/icons-material/Wc';
 import Button from '@mui/material/Button';
-import CardContent from '@mui/material/CardContent';
 import Grid from '@mui/material/Grid';
 import dayjs from 'dayjs';
 import React from 'react';
 import { useAppDispatch, useAppSelector } from '../../../hooks';
 import { toggleEditAccDetailModal } from '../src/accountReducer';
-import accountStyles from '../src/accountStyles';
 import EditAccDetailModal from './EditAccDetailModal';
 import SingleAccData from './SingleAccData';
 
@@ -19,7 +17,6 @@ interface stringDict{
 }
 
 const AccountDetails = () => {
-  const styles = accountStyles();
   const currentUserDetails = useAppSelector((state) => state.auth.currentUser);
   const genderMap:stringDict = { F: 'Female', M: 'Male' };
   const dispatch = useAppDispatch();
@@ -27,49 +24,35 @@ const AccountDetails = () => {
   return (
     <Grid container justifyContent="center" alignItems="center">
       <Grid item md={10} xs={12}>
-        <CardContent>
-          <Grid container justifyContent="center" spacing={4} className={styles.verticalSpacing}>
-            <Grid item xs={10}>
-              <Grid container justifyContent="center" alignItems="center" direction="row" spacing={5}>
-                <SingleAccData
-                  label="Full Name"
-                  data={currentUserDetails.fullName}
-                  Icon={<PersonIcon />}
-                />
-                <SingleAccData
-                  label="Gender"
-                  data={genderMap[currentUserDetails.gender]}
-                  Icon={<WcIcon />}
-                />
-              </Grid>
-            </Grid>
-            <Grid item xs={10}>
-              <Grid container justifyContent="center" alignItems="center" direction="row" spacing={5}>
-                <SingleAccData
-                  label="Email"
-                  data={currentUserDetails.email}
-                  Icon={<EmailIcon />}
-                />
-                <SingleAccData
-                  label="Phone No"
-                  data={currentUserDetails.phoneNumber}
-                  Icon={<PhoneIphoneIcon />}
-                />
-              </Grid>
-            </Grid>
-            <Grid item xs={10}>
-              <Grid container justifyContent="center" alignItems="center" direction="row" spacing={5}>
-                <SingleAccData
-                  label="Date of Birth"
-                  data={dayjs(currentUserDetails.dob).format('DD MMMM YYYY')}
-                  Icon={<CakeIcon />}
-                />
-                <Grid item xs={6} />
-              </Grid>
-            </Grid>
-          </Grid>
-        </CardContent>
-        <Grid container justifyContent="flex-end" className={styles.editBtnTopSpacing}>
+        <Grid container justifyContent="center" alignItems="center" spacing={4}>
+          <SingleAccData
+            label="Full Name"
+            data={currentUserDetails.fullName}
+            Icon={<PersonIcon />}
+          />
+          <SingleAccData
+            label="Gender"
+            data={genderMap[currentUserDetails.gender]}
+            Icon={<WcIcon />}
+          />
+          <SingleAccData
+            label="Email"
+            data={currentUserDetails.email}
+            Icon={<EmailIcon />}
+          />
+          <SingleAccData
+            label="Phone No"
+            data={currentUserDetails.phoneNumber}
+            Icon={<PhoneIphoneIcon />}
+          />
+          <SingleAccData
+            label="Date of Birth"
+            data={dayjs(currentUserDetails.dob).format('DD MMMM YYYY')}
+            Icon={<CakeIcon />}
+          />
+          <Grid item xs={6} />
+        </Grid>
+        <Grid container justifyContent="flex-end" marginTop={3}>
           <Button
             variant="contained"
             color="secondary"
