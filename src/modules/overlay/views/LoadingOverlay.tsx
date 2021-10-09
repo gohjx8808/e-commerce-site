030e67c44a9940dcbdce39f3b86554009f4bdca2
Jing Xuan Goh <jingxuan.goh@capbay.com>
2021-10-09 16:50:25 +0800

    status overlay styled

--- src/modules/overlay/views/LoadingOverlay.tsx
+++ src/modules/overlay/views/LoadingOverlay.tsx
@@ -1,23 +1,15 @@
 import CircularProgress from '@mui/material/CircularProgress';
 import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
-import { makeStyles } from '@mui/styles';
 import React from 'react';
 import { useAppSelector } from '../../../hooks';
 
-const useStyle = makeStyles({
-  whiteBg: {
-    backgroundColor: 'white',
-  },
-});
-
 const LoadingOverlay = () => {
-  const style = useStyle();
   const isLoadingOverlayOpen = useAppSelector((state) => state.overlay.isLoadingOverlayOpen);
 
   return (
     <Dialog open={isLoadingOverlayOpen}>
-      <DialogContent className={style.whiteBg}>
+      <DialogContent>
         <CircularProgress />
       </DialogContent>
     </Dialog>
