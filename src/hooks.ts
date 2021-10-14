51849d3061b46fea4e40f2bae4b1074801eedfb4
Jing Xuan Goh <jingxuan.goh@capbay.com>
2021-10-14 09:28:26 +0800

    remove unused media query hooks

--- src/hooks.ts
+++ src/hooks.ts
@@ -8,7 +8,3 @@ export const useAppSelector:TypedUseSelectorHook<RootState> = useSelector;
 
 const theme = useTheme;
 export const useXsDownMediaQuery = () => useMediaQuery(theme().breakpoints.only('xs'));
-export const useMdDownMediaQuery = () => useMediaQuery(theme().breakpoints.down('md'));
-export const useSmUpMediaQuery = () => useMediaQuery(theme().breakpoints.up('sm'));
-export const useSmDownMediaQuery = () => useMediaQuery(theme().breakpoints.down('sm'));
-export const useMdUpMediaQuery = () => useMediaQuery(theme().breakpoints.up('md'));
