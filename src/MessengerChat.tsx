4ef44913d5362195333cbce9f81658de54477fe0
Jing Xuan Goh <jingxuan.goh@capbay.com>
2021-10-26 21:17:46 +0800

    try out facebook customer chat styling

--- src/MessengerChat.tsx
+++ src/MessengerChat.tsx
@@ -21,19 +21,25 @@ function MessengerChat() {
       let js;
       const fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return;
+      // eslint-disable-next-line prefer-const
       js = d.createElement(s);
       js.id = id;
+      // @ts-ignore
       js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-      fjs.parentNode.insertBefore(js, fjs);
+      fjs.parentNode!.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
   });
   return (
     <>
       <div id="fb-root" />
       <div
+        style={{ marginBottom: 300 }}
         className="fb-customerchat"
+        // @ts-ignore
         attribution="page_inbox"
         page_id="1927588117459422"
+        desktop_bottom_spacing={200}
+        mobile_bottom_spacing={100}
       />
     </>
   );
