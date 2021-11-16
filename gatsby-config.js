13e406aab0f92d9cbc4d4203510f71c009381370
Jing Xuan Goh <jingxuan.goh@capbay.com>
2021-11-16 22:42:56 +0800

    added nprogress for loading between pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -90,18 +90,16 @@ module.exports = {
       resolve: 'gatsby-plugin-google-tagmanager',
       options: {
         id: process.env.GTM_ID,
-
-        // Include GTM in development.
-        //
-        // Defaults to false meaning GTM will only be loaded in production.
         includeInDevelopment: false,
-
-        // datalayer to be set before GTM is loaded
-        // should be an object or a function that is executed in the browser
-        //
-        // Defaults to null
         defaultDataLayer: { platform: 'gatsby' },
         enableWebVitalsTracking: true,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-nprogress',
+      options: {
+        color: '#f5dbc9',
+        showSpinner: true,
       },
     },
   ],
