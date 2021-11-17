3339a30bb7bf1990a21ec9964326ad0704324454
Jing Xuan Goh <jingxuan.goh@capbay.com>
2021-11-17 10:58:12 +0800

    trying loading overlay on initial loading

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,8 +2,13 @@ import React from 'react';
 import RootLayout from './src/layouts/RootLayout';
 import WrapPageElement from './src/WrapPageElement';
 import '@fontsource/amaranth';
+import { toggleLoadingOverlay } from './src/modules/overlay/src/overlayReducer';
 
 // eslint-disable-next-line react/jsx-filename-extension
 export const wrapRootElement = ({ element }) => <RootLayout>{element}</RootLayout>;
 
 export const wrapPageElement = WrapPageElement;
+
+export const onInitialClientRender = () => {
+  toggleLoadingOverlay(true);
+};
