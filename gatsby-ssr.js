d9dab17cbd8bcea6e13cd6966421c5af04788c3f
Jing Xuan Goh <jingxuan.goh@capbay.com>
2021-11-21 14:29:25 +0800

    fix login & signup style corrupt after refresh

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,4 +6,6 @@ import '@fontsource/amaranth';
 // eslint-disable-next-line react/jsx-filename-extension
 export const wrapRootElement = ({ element }) => <RootLayout>{element}</RootLayout>;
 
-export const wrapPageElement = WrapPageElement;
+export const wrapPageElement = ({ element, props }) => (
+  <WrapPageElement {...props}>{element}</WrapPageElement>
+);
