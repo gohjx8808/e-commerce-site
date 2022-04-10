055df71ab20d0218c1d010961e2861f3547d4ce4
Jing Xuan Goh <jingxuan.goh@capbay.com>
2022-04-10 11:17:08 +0800

    forgot password revamp

--- src/modules/auth/src/authQueries.ts
+++ src/modules/auth/src/authQueries.ts
@@ -1,5 +1,8 @@
+import { navigate } from "gatsby";
+import firebase from "gatsby-plugin-firebase";
 import { useMutation, useQuery } from "react-query";
 import { useAppDispatch } from "../../../hooks";
+import routeNames from "../../../utils/routeNames";
 import { toggleLoadingOverlay } from "../../overlay/src/overlayReducer";
 import {
   toggleStatusModal,
@@ -7,7 +10,7 @@ import {
   updateStatusMsg,
   updateStatusTitle,
 } from "../../status/src/statusReducer";
-import { getCurrentUserDetails, signOut } from "./authApis";
+import { getCurrentUserDetails, resetPassword, signOut } from "./authApis";
 import { uidStorageKey } from "./authConstants";
 
 export const getCurrentUserDetailsKey = "getCurrentUserDetails";
@@ -56,4 +59,36 @@ export const useUserDetails = (onAdditionalSuccess?: () => void) => {
       staleTime: Infinity,
     }
   );
+};
+
+export const useForgotPassword = () => {
+  const dispatch = useAppDispatch();
+
+  return useMutation("submitForgotPassword", resetPassword, {
+    onSuccess: () => {
+      dispatch(toggleSuccess(true));
+      dispatch(
+        updateStatusMsg(
+          "An email to reset your password has been sent to your registered email address."
+        )
+      );
+      dispatch(toggleLoadingOverlay(false));
+      dispatch(toggleStatusModal(true));
+      navigate(routeNames.login);
+    },
+    onError: (error: firebase.auth.Error) => {
+      if (error.code === "auth/user-not-found") {
+        dispatch(
+          updateStatusMsg(
+            "The email address is not registered. Please insert a registered email address!"
+          )
+        );
+      } else {
+        dispatch(updateStatusMsg("Network error! Please try again."));
+      }
+      dispatch(toggleSuccess(false));
+      dispatch(toggleLoadingOverlay(false));
+      dispatch(toggleStatusModal(true));
+    },
+  });
 };
--- src/pages/forgotPassword.tsx
+++ src/pages/forgotPassword.tsx
@@ -1,22 +1,21 @@
-import { yupResolver } from '@hookform/resolvers/yup';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import CardContent from '@mui/material/CardContent';
-import Grid from '@mui/material/Grid';
-import Link from '@mui/material/Link';
-import Typography from '@mui/material/Typography';
-import { graphql, Link as GatsbyLink, useStaticQuery } from 'gatsby';
-import { getImage } from 'gatsby-plugin-image';
-import React, { useCallback } from 'react';
-import { useForm } from 'react-hook-form';
-import { useAppDispatch } from '../hooks';
-import ControlledTextInput from '../sharedComponents/inputs/ControlledTextInput';
-import { AuthCard, AuthCardHeader, AuthIcon } from '../styledComponents/auth';
-import routeNames from '../utils/routeNames';
-import { submitForgotPassword } from '../modules/auth/src/authReducer';
-import { forgotPasswordSchema } from '../modules/auth/src/authSchema';
-import AuthLayout from '../layouts/AuthLayout';
+import { yupResolver } from "@hookform/resolvers/yup";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import LoadingButton from "@mui/lab/LoadingButton";
+import Box from "@mui/material/Box";
+import CardContent from "@mui/material/CardContent";
+import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+import { graphql, Link as GatsbyLink, useStaticQuery } from "gatsby";
+import { getImage } from "gatsby-plugin-image";
+import React from "react";
+import { useForm } from "react-hook-form";
+import AuthLayout from "../layouts/AuthLayout";
+import { useForgotPassword } from "../modules/auth/src/authQueries";
+import { forgotPasswordSchema } from "../modules/auth/src/authSchema";
+import ControlledTextInput from "../sharedComponents/inputs/ControlledTextInput";
+import { AuthCard, AuthCardHeader, AuthIcon } from "../styledComponents/auth";
+import routeNames from "../utils/routeNames";
 
 const ForgotPassword = () => {
   const data = useStaticQuery(graphql`
@@ -30,22 +29,32 @@ const ForgotPassword = () => {
   `);
 
   const image = getImage(data.file);
-  const dispatch = useAppDispatch();
+  const { mutate: submitForgotPassword, isLoading: forgotPasswordLoading } =
+    useForgotPassword();
 
-  const { control, formState: { errors }, handleSubmit } = useForm({
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
     resolver: yupResolver(forgotPasswordSchema),
   });
 
-  const submitLogin = useCallback((hookData:auth.submitForgotPasswordPayload) => {
-    dispatch(submitForgotPassword(hookData));
-  }, [dispatch]);
+  const submitLogin = (hookData: auth.submitForgotPasswordPayload) => {
+    submitForgotPassword(hookData);
+  };
 
   return (
     <AuthLayout>
       <Grid item xs={12}>
         <Grid container justifyContent="center" alignItems="center" marginY={1}>
           <Grid item xs={10} sm={9} lg={6}>
-            <Link component={GatsbyLink} to={routeNames.login} color="textPrimary" underline="hover">
+            <Link
+              component={GatsbyLink}
+              to={routeNames.login}
+              color="textPrimary"
+              underline="hover"
+            >
               <Grid container>
                 <ArrowBackIcon />
                 <Typography>Back to Login</Typography>
@@ -56,16 +65,29 @@ const ForgotPassword = () => {
         <Grid container justifyContent="center" alignItems="center">
           <Grid item xs={10} sm={9} lg={6}>
             <AuthCard>
-              <Grid container justifyContent="center" alignItems="center" direction="column">
+              <Grid
+                container
+                justifyContent="center"
+                alignItems="center"
+                direction="column"
+              >
                 <AuthCardHeader title="Forgot Password" />
-                <Box width={{ xs: '35%', sm: '20%' }}>
+                <Box width={{ xs: "35%", sm: "20%" }}>
                   <AuthIcon image={image!} alt="icon" />
                 </Box>
               </Grid>
               <CardContent>
                 <form onSubmit={handleSubmit(submitLogin)}>
-                  <Grid container justifyContent="center" alignItems="center" spacing={2} marginTop={1}>
-                    <Typography variant="subtitle1" color="white">Please enter your registered email address below.</Typography>
+                  <Grid
+                    container
+                    justifyContent="center"
+                    alignItems="center"
+                    spacing={2}
+                    marginTop={1}
+                  >
+                    <Typography variant="subtitle1" color="white">
+                      Please enter your registered email address below.
+                    </Typography>
                     <Grid item xs={12} sm={10}>
                       <ControlledTextInput
                         control={control}
@@ -76,11 +98,23 @@ const ForgotPassword = () => {
                       />
                     </Grid>
                   </Grid>
-                  <Grid container justifyContent="center" alignItems="center" marginTop={3} marginBottom={2}>
+                  <Grid
+                    container
+                    justifyContent="center"
+                    alignItems="center"
+                    marginTop={3}
+                    marginBottom={2}
+                  >
                     <Grid item xs={6} sm={3}>
-                      <Button variant="contained" color="primary" type="submit" fullWidth>
+                      <LoadingButton
+                        variant="contained"
+                        color="primary"
+                        type="submit"
+                        fullWidth
+                        loading={forgotPasswordLoading}
+                      >
                         Submit
-                      </Button>
+                      </LoadingButton>
                     </Grid>
                   </Grid>
                 </form>
