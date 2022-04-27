37c675985a0422ef1bb7404fd223a4b5893ec7d6
Jing Xuan Goh <jingxuan.goh@capbay.com>
2022-04-27 09:59:12 +0800

    removed unused image gallery reducer

--- src/rootReducer.ts
+++ src/rootReducer.ts
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
 import { statusSlice } from './modules/status/src/statusReducer';
-import { imageGallerySlice } from './modules/imageGallery/src/imageGalleryReducer';
 
 export default combineReducers({
   status: statusSlice.reducer,
-  imageGallery: imageGallerySlice.reducer,
 });
