203fee87daf6ea8016af4c42fbf0c282e4cf7ad0
jingxuangoh <gohjx8808@gmail.com>
2023-01-25 12:48:32 +0800

    dynamic api url

--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -1,6 +1,9 @@
 import axios, { AxiosResponse } from "axios";
 
-const API_URL= "https://yjartjournal-api.vercel.app"
+const API_URL =
+  process.env.NODE_ENV === "PRODUCTION"
+    ? "https://yjartjournal-api.vercel.app"
+    : "http://localhost:3000";
 
 export const postRequest = <T>(
   url: string,
